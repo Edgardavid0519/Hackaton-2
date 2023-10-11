ad9570a8260f3b11d29927663102010641973429
Mayra Troyano <mayra-troyano@hotmail.com>
2023-10-11 13:47:30 -0500

    ingreso de datos a la Rama Contacto

--- AgendaTelefonica/src/Contacto.java
+++ AgendaTelefonica/src/Contacto.java
@@ -1,2 +1,31 @@
 public class Contacto {
+    //Inicializamos los atributos
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    //inicializamos constrctor
+    public Contacto(String nombre, String apellido, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+    //Inicializamos getter y setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
