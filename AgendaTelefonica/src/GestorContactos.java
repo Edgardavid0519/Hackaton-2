cd73ebc7e61e7398ff9c6fef4d286e331ad97996
Mayra Troyano <mayra-troyano@hotmail.com>
2023-10-11 19:52:14 -0500

    solucion1

--- AgendaTelefonica/src/GestorContactos.java
+++ AgendaTelefonica/src/GestorContactos.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
 public class GestorContactos {
+    private List<Contacto> contactos = new ArrayList<>();
+    public boolean agregarContacto(Contacto contacto) {
+        if (!existeContacto(contacto)) {
+
+            contactos.add(contacto);
+            return true;
+        }
+        return false;
+    }
+    public boolean existeContacto(Contacto contacto) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(contacto.getNombre()) && (c.getApellido().equalsIgnoreCase(contacto.getApellido()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public List<Contacto> listarContactos() {
+        return contactos;
+    }
+
+    public boolean buscarContacto(String nombre) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equals(nombre)) {
+                System.out.println("\nNombre: " + c.getNombre() + ", Apellido: " + c.getApellido() + ", Teléfono: " + c.getTelefono());
+                return true;
+            }
+        }
+        System.out.println("\nNo se encuentra");
+        return false;
+    }
+
+    public boolean eliminarContacto(String nombre) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                contactos.remove(c);
+                System.out.println("\nEl contacto" + c.getNombre() + " " + c.getApellido() +" se ha eliminado.");
+                return true;
+            }
+        }
+        System.out.println("No se encuentra");
+        return false;
+    }
+    public boolean agendaLlena() {
+        return contactos.size() >= 20;
+    }
+    public int espacioLibre() {
+        return 20 - contactos.size();
+    }
 }
--- AgendaTelefonica/src/Main.java
+++ AgendaTelefonica/src/Main.java
@@ -1,7 +1,67 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        GestorContactos agenda = new GestorContactos();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("\nMenu contactos:");
+            System.out.println("1. Agregar contacto");
+            System.out.println("2. Buscar contacto");
+            System.out.println("3. Lista de contactos");
+            System.out.println("4. Eliminar contacto");
+            System.out.println("5. Espacio libre en la agenda");
+            System.out.print("Selecciona una opción: ");
+
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Nombre: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Apellido: ");
+                    String apellido = scanner.nextLine();
+                    System.out.print("Celular: ");
+                    String telefono = scanner.nextLine();
+
+                    Contacto nuevoContacto = new Contacto(nombre, apellido, telefono);
+                    if (agenda.agregarContacto(nuevoContacto)) {
+                        do {
+                            System.out.print("ingrese de nuevo el celular: ");
+                            telefono = scanner.nextLine();
+                        } while (telefono.length() != 10);
+
+                        System.out.println("\nContacto " + nombre +  " " + apellido + " agregado");
+                    } else {
+                        System.out.println("\nEl contacto ya existe");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("\nIngrese nombre a buscar: ");
+                    String nombreBuscar = scanner.nextLine();
+                    agenda.buscarContacto(nombreBuscar);
+                    break;
+
+                case 3:
+                    System.out.println("\nContactos en la agenda:");
+                    for (Contacto contacto : agenda.listarContactos()) {
+                        System.out.println("\nNombre: " + contacto.getNombre() + ", Teléfono: " + contacto.getTelefono());
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("\nNombre del contacto a eliminar: ");
+                    String nombreEliminar = scanner.nextLine();
+                    agenda.eliminarContacto(nombreEliminar);
+                    break;
+
+                case 5:
+                    System.out.println("\nEspacio libre en la agenda: " + agenda.espacioLibre());
+                    break;
+            }
+        }
     }
 }
